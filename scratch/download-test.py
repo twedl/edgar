9f94b1d66f10702d42b065af8a14bf54cd57ae29
tweed1e <jesse.tweedle@gmail.com>
2022-01-25 17:22:37 -0500

    first commit

---
+++ scratch/download-test.py
@@ -0,0 +1,10 @@
+# python script to see test how to download the financial documents
+
+# first, get a url
+
+# second, download the file to a temp dir
+
+# third, unzip, use fs to get all the files inside
+
+# fourth, use...idk what to figure out what's inside. not pandas
+#   xml explorer maybe
